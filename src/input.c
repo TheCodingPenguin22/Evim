cee1292c8c44250af8bc09b765dcbf9157a19308
AdolfssonFredrik <fredik.adolfsson02@gmail.com>
2025-05-09 10:45:30 +0200

    fixed save as

--- src/input.c
+++ src/input.c
@@ -166,7 +166,10 @@ char* editorPrompt(char* promt){
     editorRefreshScreen();
 
     int c = editorReadKey();
-    if(c == '\x1b') {
+    if(c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE){
+      if (buflen != 0) buf[--buflen] = '\0';
+    }
+    else if(c == '\x1b') {
       editorSetStatusMessage("",0);
       free(buf);
       return NULL;
