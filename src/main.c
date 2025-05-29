e4f9c669d8922b05a6b215ed2371f528db545c6b
AdolfssonFredrik <fredik.adolfsson02@gmail.com>
2025-05-29 21:23:13 +0200

    made cursor change based on mode

--- src/main.c
+++ src/main.c
@@ -1,30 +1,31 @@
 
-#include "terminal.h"
-#include "init.h"
+#include "data.h"
 #include "file_io.h"
-#include "output.h"
+#include "init.h"
 #include "input.h"
+#include "output.h"
+#include "terminal.h"
 
-// Main function, takes in arguments from the cli. 
-int main(int argc, char *argv[])
-{
+// Main function, takes in arguments from the cli.
+int main(int argc, char *argv[]) {
   enableRawMode();
   initEditor();
 
-  if(argc >= 2) {
+  if (argc >= 2) {
     editorOpen(argv[1]);
   }
 
   editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
-  
-  while (1){
+
+  while (1) {
     editorRefreshScreen();
-    if(E.currentMode == NORMAL_MODE) editorProcessKeypressNormalMode();
-    if(E.currentMode == INSERT_MODE) editorProcessKeypressInserMode();
-    if(E.currentMode == COMMAND_MODE) editorProcessKeypressCommandMode();
+    if (E.currentMode == NORMAL_MODE || E.currentMode == DELETE_MODE)
+      editorProcessKeypressNormalMode();
+    if (E.currentMode == INSERT_MODE)
+      editorProcessKeypressInserMode();
+    if (E.currentMode == COMMAND_MODE)
+      editorProcessKeypressCommandMode();
     editorRefreshScreen();
   }
   return 0;
 }
-
-
